947fe93259f5e993a18aad894bbb8449dea45d06
Ravin Kumar <kumar.ravin444@gmail.com>
2022-07-15 15:52:43 -0300

    creating weather prediction and starting

---
+++ SLM.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random
+
+
+def read_passage(path):
+    with open(path, 'r') as passage:
+        passage_text = passage.read()
+    return passage_text
+
+def parse_text(text):
+    
+
+class TransitionMatrix():
+
+    def __init__(self, text):
+
+        num_words = len(set(' '.split(text)))
+        matrix = np.zeros((num_words, num_words))
